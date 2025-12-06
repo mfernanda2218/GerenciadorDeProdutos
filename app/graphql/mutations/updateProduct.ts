97f2dc9555f7516f0726c748125c28bac4b3ba52
MariaFernanda <mafaduartemelot216@gmail.com>
2025-12-06 12:16:46 -0300

    refactor: ajuste na mutation

--- app/graphql/mutations/updateProduct.ts
+++ app/graphql/mutations/updateProduct.ts
@@ -1,11 +1,21 @@
 // graphql/mutations/updateProduct.ts
 import { gql } from '@apollo/client'
 
+/**
+ * @param {string} $id - ID do produto a ser atualizado
+ * @param {UpdateProductInput} $input - Campos do produto a serem atualizados
+ * @returns {Product} - O produto atualizado
+ */
 export const UPDATE_PRODUCT = gql`
   mutation UpdateProduct($id: ID!, $input: UpdateProductInput!) {
     updateProduct(id: $id, input: $input) {
       id
       name
+      supplier
+      salePrice
+      costPrice
+      quantity
+      updatedAt
     }
   }
 `
